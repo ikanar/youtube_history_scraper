7adc948e692c73dfcb4333f566e2b630a69bf4d2
ikanar <ian.kanar@gmail.com>
2024-01-12 21:50:18 -0500

    Update youtube_history_scraper.py
    Added quit button
    Added search length entry
    Added file name entry
    got rid of printing to terminal

--- youtube_history_scraper.py
+++ youtube_history_scraper.py
@@ -7,14 +7,19 @@ import tkinter as tk
 
 
 
+
+
 #REQUIRES YOU TO USE YOUR PHONE TO AUTHENTICATE THE USER
-#GOOGLE WILL SEND YOU AN ALERT AND YOU HAVE 30 SECONDS TO CLICK YES OR THE PROGRAM WILL FAIL
+#GOOGLE WILL SEND YOU AN ALERT AND YOU HAVE 10 SECONDS TO CLICK YES OR THE PROGRAM WILL FAIL
 #PROGRAM ALSO WILL THROW ERROR AT THE END THIS IS A KNOWN BUG WITH UNDETECTED CHROME DRIVER AND IT IS WORKING AS INTENDED FOR NOW.
 
 
-def scrape_history(username,password,scan_length):
+def scrape_history(username,password,scan_length,file_name):
     driver = uc.Chrome()
     driver.get('https://accounts.google.com/')
+    scan_length_int = int(scan_length)
+
+    
 
     #handles login to google account
     #sleeps inbetween each stage to allow time to load
@@ -23,15 +28,16 @@ def scrape_history(username,password,scan_length):
     sleep(7)
     driver.find_element(By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)
     driver.find_element(By.XPATH, '//*[@id="passwordNext"]/div/button/span').click()
-    sleep(7)
+    sleep(10)
     driver.get('https://www.youtube.com/feed/history')
 
-    #scans and collects html elements
-    while scan_length > 0:
+    #scans and collects the html elements
+    
+    while scan_length_int > 0:
         elem = driver.find_element(By.TAG_NAME, "html")
         elem.send_keys(Keys.END)
         sleep(5)
-        scan_length = scan_length - 1
+        scan_length_int = scan_length_int - 1
     
     elements = driver.find_elements(By.XPATH,'//*[@id="thumbnail"]')
 
@@ -45,10 +51,9 @@ def scrape_history(username,password,scan_length):
             links.append(e)
         
 
-    #prints out links
-    file = open("youtube_history.txt","w")
+    #prints out links to a file
+    file = open(file_name + ".txt","w")
     for link in links:
-        print(link)
         file.write(link+"\n")
     file.close()    
     
@@ -59,51 +64,55 @@ def scrape_history(username,password,scan_length):
 
 
 def Search():
+    #gathers the data from the UI and then uses it to call scrape_history
     email = email_entry.get()
     password = password_entry.get()
-    scrape_history(email,password,2)
+    scan_length = scan_length_entry.get()
+    file_name = file_name_entry.get()
+    scrape_history(email,password,scan_length,file_name)
 
 
 
 if __name__ == '__main__':
-    #gets username and password from user
-    #username = input("Enter google Username:")
-    #password = input("Enter password:")
+    #UI initialization 
 
+    root = tk.Tk()
 
-    UI = tk.Tk()
+    root.geometry("400x150")
 
-    UI.geometry("600x400")
+    root.title("Youtube History Scraper")
 
-    email_label = tk.Label(UI,text= "Enter Email")
+    email_label = tk.Label(root,text= "Enter Email")
+    email_entry= tk.Entry(root,font=('calibre',10,'normal'))
 
-    email_entry= tk.Entry(UI,font=('calibre',10,'normal'))
+    password_label = tk.Label(root,text= "Enter Password")
+    password_entry = tk.Entry(root,font=('calibre',10,"normal"),show='*')
 
-    password_label = tk.Label(UI,text= "Enter Password")
-
-    password_entry = tk.Entry(UI,font=('calibre',10,"normal"),show='*')
-
+    scan_length_label = tk.Label(root,text="Enter Integer scan length")
+    scan_length_entry = tk.Entry(root,font=('calibre',10,"normal"))
     
-    button = tk.Button(UI,text = "Search", command = Search )
+    file_name_label = tk.Label(root,text="Enter name of save File")
+    file_name_entry = tk.Entry(root,font=('calibre',10,"normal"))
 
+    quit_button = tk.Button(root, text='Quit', command=root.destroy)
+    search_button = tk.Button(root,text = "Search", command = Search )
     
+
+
     email_label.grid(row=0,column=0)
     email_entry.grid(row=0,column=1)
+
     password_label.grid(row=1,column=0)
     password_entry.grid(row=1,column=1)
-    button.grid(row=2,column=1)
+
+    scan_length_label.grid(row=2,column=0)
+    scan_length_entry.grid(row=2,column=1)
+    
+    file_name_label.grid(row=3,column=0)
+    file_name_entry.grid(row=3,column=1)
+    
+    quit_button.grid(row=4,column =0)
+    search_button.grid(row=4,column=1)
     
     
-    
-    
-    UI.mainloop()
-
-
-
-
-
-    
-
-
-
-    history = scrape_history(username,password,2)
+    root.mainloop()
